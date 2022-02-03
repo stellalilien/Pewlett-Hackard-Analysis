3da41701acc64a311a607f0222a0e9918bf752e3
stellalilien <stella.cooley@gmail.com>
2022-02-03 12:33:05 -0800

    updated titles primary key

--- schema.sql
+++ schema.sql
@@ -46,5 +46,5 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date)
 );
